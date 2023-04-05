bb983081b872d4af7ecc5b24ac9e16c03e15daa6
Greenbel Eleghasim <obisann@gmail.com>
2023-04-05 16:06:16 +0100

    edited install_mysql.sh

---
+++ config_git.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# Script for setting git config
+
+git config --global user.name "Greenbel Eleghasim"
+git config --global user.email "obisann@gmail.com"
--- install_mysql.sh
+++ install_mysql.sh
@@ -4,7 +4,7 @@
 apt-key adv --keyserver pgp.mit.edu --recv-keys 3A79BD29  # get GPG keys
 
 
-echo "deb http://repo.mysql.com/apt/ubuntu/ bionic mysql-8.0" >> etc/apt/sources.list.d/mysql.list  # register repo for mysql-8.0 resource; format: deb http://repo.mysql.com/apt/{debian|ubuntu}/ {buster|bionic} {mysql-5.7|mysql-8.0|workbench-8.0|connector-python-8.0}
+echo "deb http://repo.mysql.com/apt/ubuntu/ bionic mysql-8.0" >> /etc/apt/sources.list.d/mysql.list  # register repo for mysql-8.0 resource; format: deb http://repo.mysql.com/apt/{debian|ubuntu}/ {buster|bionic} {mysql-5.7|mysql-8.0|workbench-8.0|connector-python-8.0}
 
 apt-get update  # so that the newly added repo can reflect
 
